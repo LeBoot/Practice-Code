9129a761dce362b8418ad07c918a53f7ab7a91d3
LeBoot <brlebout@gmail.com>
2020-03-13 15:54:38 -0400

    End of week 3/13/20

--- HackerRankChallenges/src/main/java/bl/hackerrankchallenges/App.java
+++ HackerRankChallenges/src/main/java/bl/hackerrankchallenges/App.java
@@ -5,12 +5,6 @@
  */
 package bl.hackerrankchallenges;
 
-import bl.hackerrankchallenges.hGreedy.GreedyFlorist;
-import bl.hackerrankchallenges.hSearch.Candy;
-import bl.hackerrankchallenges.hSearch.IceCream;
-import bl.hackerrankchallenges.hSearch.MinimumTime;
-import bl.hackerrankchallenges.hStringManipulation.SpecialString;
-
 
 /**
  *
@@ -18,15 +12,8 @@ import bl.hackerrankchallenges.hStringManipulation.SpecialString;
  */
 public class App {
     
-    static Candy candy = new Candy();
-    
     public static void main(String[] args) {
-        long m = 1;
-        long w = 2;
-        long p = 1;
-        long n = 60;
-        candy.runCandy(m, w, p, n);
-        
+
     }  
     
 }
